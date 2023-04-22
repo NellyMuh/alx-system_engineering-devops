a2ec1fc99c2834b184d41b5f6ad23e411e93924d
NellyMuh <m.nelly704@gmail.com>
2023-04-21 21:01:36 -0700

    file created

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# installs the package puppet-lint
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep 2.1.0',
+}
