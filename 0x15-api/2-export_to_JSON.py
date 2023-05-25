11d6efe290eb40160281d77badd1b607bf3e224c
NellyMuh <m.nelly704@gmail.com>
2023-05-25 15:29:37 -0700

    edited file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""Exports data in the JSON format"""
+'''A script that gathers data from an API and exports it to a CSV file.
+'''
+import re
+import requests
+import sys
 
-if __name__ == "__main__":
 
-        import json
-        import requests
-        import sys
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
 
-         userId = sys.argv[1]
-         user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                 .format(userId))
-         todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-         todos = todos.json()
 
-         todoUser = {}
-         taskList = []
-
-         for task in todos:
-             if task.get('userId') == int(userId):
-                 taskDict = {"task": task.get('title'),
-                         "completed": task.get('completed'),
-                         "username": user.json().get('username')}
-                 taskList.append(taskDict)
-         todoUser[userId] = taskList
-
-         filename = userId + '.json'
-         with open(filename, mode='w') as f:
-             json.dump(todoUser, f)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('username')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            with open('{}.csv'.format(id), 'w') as file:
+                for todo in todos:
+                    file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            id,
+                            user_name,
+                            todo.get('completed'),
+                            todo.get('title')
+                        )
+                    )
