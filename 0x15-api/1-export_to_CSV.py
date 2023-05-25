589772bdba2e3ca079071f2f8d1fa7b869cd5925
NellyMuh <m.nelly704@gmail.com>
2023-05-25 15:24:22 -0700

    edited file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-"""Exports data in the CSV format"""
+'''A script that gathers data from an API and exports it to a CSV file.
+'''
+import re
+import requests
+import sys
 
-if __name__ == "__main__":
 
-        import csv
-        import requests
-        import sys
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
 
-        userId = sys.argv[1]
-        user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                .format(userId))
-        name = user.json().get('username')
-        todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-        filename = userId + '.csv'
-        with open(filename, mode='w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"',
-                quoting=csv.QUOTE_ALL, lineterminator='\n')
-        for task in todos.json():
-            if task.get('userId') == int(userId):
-                writer.writerow([userId, name, str(task.get('completed')),
-                    task.get('title')])
+if __name__ == '__main__':
+        if len(sys.argv) > 1:
+                    if re.fullmatch(r'\d+', sys.argv[1]):
+                                    id = int(sys.argv[1])
+                                                user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+                                                            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+                                                                        user_name = user_res.get('username')
+                                                                                    todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+                                                                                                with open('{}.csv'.format(id), 'w') as file:
+                                                                                                                    for todo in todos:
+                                                                                                                                            file.write(
+                                                                                                                                                                            '"{}","{}","{}","{}"\n'.format(
+                                                                                                                                                                                                            id,
+                                                                                                                                                                                                                                        user_name,
+                                                                                                                                                                                                                                                                    todo.get('completed'),
+                                                                                                                                                                                                                                                                                                todo.get('title')
+                                                                                                                                                                                                                                                                                                                        )
+                                                                                                                                                                                                )
